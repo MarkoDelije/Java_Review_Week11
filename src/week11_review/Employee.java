865277024cb21d93ef3e5fd7e8045cd193edc738
Marko'sMac <Markolucas93@yahoo.com>
2022-12-10 12:30:02 -0500

    Employee Week 11 Review Class

---
+++ src/week11_review/Employee.java
@@ -0,0 +1,67 @@
+package week11_review;
+
+public class Employee extends Person{
+
+    private String employeeId,jobTitle;
+    private double salary;
+
+
+    public Employee(String name, int age, char gender, String employeeId, String jobTitle, double salary) {
+        super(name, age, gender);
+        setEmployeeId(employeeId);
+        setJobTitle(jobTitle);
+        setSalary(salary);
+
+    }
+    // Constructor from the parent class with added additional variables from the child class
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        if(jobTitle == null){
+            System.err.println("Invalid job title");
+            System.exit(1);
+        }
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        if(salary < 0){
+            System.err.println("Salary can not be negative");
+            System.exit(1);
+        }
+        if(salary == 0){
+            System.err.println("salary can not be zero");
+            System.exit(1);
+        }
+        this.salary = salary;
+    }
+
+    public void work(){
+        System.out.println(getName() + " is working");
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()+ "{" +
+                super.toString() +
+                ", employeeId='" + employeeId + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
